52c985d2d0811a07db614543165b90d4ad286181
Pavel Valena <pvalena@redhat.com>
2018-07-19 16:42:00 +0200

    pelc.sh: refactor a bit, also add ignore capability and comments

--- pkgs/pelc.sh
+++ pkgs/pelc.sh
@@ -29,6 +29,7 @@
 #     -d        include debuging output
 #     -h        show this help
 #     -g        package type: gem
+#     -i        ignore non-fatal errors
 #     -p        package type: ???                             # <<< [placeholder]
 #     -r  RHL   rhel version
 #     -w  WID   columnes width, starts \w #2, sep. by ','
@@ -46,48 +47,36 @@
 #
 # TODO:
 #     - output in html
-#     - check oscrypto
+#     - enable oscrypto
 #
 # Ideas:
-#     - output csv format
 #     - Use https://github.com/nexB/scancode-toolkit
 #        ./scancode  --license --processes 2
+#     - Type autodetection
 #
 # Author:
 #     Pavel Valena <pvalena@redhat.com>
 #
 #
 
+trap 'kill 0 ; exit 0' SIGTERM
+trap 'kill 0 ; exit 0' SIGINT
+trap 'kill 0 ; exit 0' SIGABRT
+
 set -e
 bash -n "$0"
 
-stx () { [[ "$DEBUG" ]] || return 0 ; echo "set -x" ; }
-err () { `stx` ; printf "!! $x: $1\n" >&2 ; }
-die () { `stx` ; [[ -n "$2" ]] && { git status || : ; } || { usage n | cat ; } ; err "$1" ; exit 1 ; }
-deb () { [[ "$DEBUG" ]] || return 0 ; echo -e " # $x : $@" ; }
-nam () { x="`grep -q '^$pre' <<< "$1" && cut -d'-' -f2- <<< "$1" || echo "$1"`" ; }
-
-OUT=''
+# globals
+OUT=
 w0=0
-put () {
-  echo "$OUT"
-  OUT=''
-  w0=0
-}
+IGN=
+DEB=
 
-usage () {
-  local mf="`readlink -e "$0"`"
-  [[ -r "$mf" ]] || die "$LINENO: Invalid file "
-
-  local N="`cat -n "$mf" | tr -s '\t' ' ' | grep -vE '^ [0-9]+ #' | head -n1 | cut -f2 -d' ' | grep -E '^[0-9]+$'`"
-
-  [[ -n "$N" && $N -gt 2 ]] || die 'No help :('
-
-  let 'N -= 1'
-
-  head -n$N "$mf" | tail -n+2 | cut -d'#' -f2- | ${PAGER-more}
-  [[ -n "$1" ]] || exit 0
-}
+stx () { [[ -z "$DEB" ]] || echo "set -x" ; }
+err () { `stx` ; [[ -n "$IGN" && -z "$1" ]] || printf "!! $x: $1\n" >&2 ; }
+die () { `stx` ; [[ -n "$2" ]] && { git status || : ; } || { usage n | cat ; } ; err "$1" f ; exit 1 ; }
+deb () { [[ -z "$DEB" ]] || echo -e " # $x : $@" ; }
+nam () { x="`grep -q '^$pre' <<< "$1" && cut -d'-' -f2- <<< "$1" || echo "$1"`" ; }
 
 out () {
   [[ "$1" ]] || out ' '
@@ -100,13 +89,30 @@ out () {
   done
 }
 
+put () {
+  echo "$OUT"
+  OUT=''
+  w0=0
+}
+
+usage () {
+  local mf="`readlink -e "$0"`"
+  [[ -r "$mf" ]] || die "$LINENO: Invalid file "
+  local N="`cat -n "$mf" | tr -s '\t' ' ' | grep -vE '^ [0-9]+ #' | head -n1 | cut -f2 -d' ' | grep -E '^[0-9]+$'`"
+  [[ -n "$N" && $N -gt 2 ]] || die 'No help :('
+  let 'N -= 1'
+  head -n$N "$mf" | tail -n+2 | cut -d'#' -f2- | ${PAGER-more}
+  [[ -n "$1" ]] || exit 0
+}
+
 [[ "$1" == "--help" ]] && { usage ; } || :
 
 [[ "$1" == "-b" ]] && { shift ; b="$1" ; shift ; } || b='rhel-8.0'
-[[ "$1" == "-c" ]] && { c='yes' ; shift ; } || c=
-[[ "$1" == "-d" ]] && { DEBUG="$1" ; shift ; } || DEBUG=
-[[ "$1" == "-g" ]] && { shift ; TG="$1" ; } || TG=
+[[ "$1" == "-c" ]] && { shift ; c='yy' ; } || c=
+[[ "$1" == "-d" ]] && { shift ; d='yy' ; } || d=
+[[ "$1" == "-g" ]] && { shift ; g="yy" ; } || g=
 [[ "$1" == "-h" ]] && { usage ; } || :
+[[ "$1" == "-i" ]] && { shift ; i='yy' ; } || i=
 [[ "$1" == "-p" ]] && { shift ; p="$1" ; } || p=
 [[ "$1" == "-r" ]] && { shift ; r="$1" ; shift ; } || r=8
 [[ "$1" == "-w" ]] && { shift ; w="$1" ; shift ; } || w=30,30,40,30
@@ -114,42 +120,45 @@ out () {
 [[ "${1:0:1}" != '-' ]] || die "$LINENO: Unknown arg or invalid order: '$1'" n
 [[ "$1" ]] || die "$LINENO: Please specify PKG" n
 
-myd="$(readlink -e "`pwd`")"
-fst="$c"                    # runs `rhpkg co` checkout, git checkout etc. if set to nonempty
+# Set Vars
+fst="$c"
+bra="$b"
+TG="$g"
 w="0,$w,0"
+IGN="$i"
+DEB="$d"
+myd="$(readlink -e "`pwd`")"
 mydown="$(readlink -e "`dirname "$0"`/download-builds.sh")"
-bra="${b}"
 
+# Sanity checks.
+[[ -x "$mydown" ]] || die 'No download-builds.sh found: $mydown' n
+[[ -n "$myd" && -d "$myd" ]] || die "$LINENO: Invalid working directory" n
+
+# Determine packages type
+T=
+[[ -z "$TG" ]] || { T="gemspec" ; pre='rubygem-' ; }
+[[ -z "$p" ]] || { T="" ; pre='???-' ; }          # <<< [placeholder]
+[[ -n "$T" ]] || die 'No package type specified'
+
+# Check if SCL enablement is needed.
 licensee &>/dev/null && isrhel= || isrhel='scl enable rh-ruby24 --'
 
+# Set w1,w2,w3...
 for i in {1..100}; do
   eval "w$i='`cut -d',' -f$i <<< "$w"`'"
 done
 
-[[ -x "$mydown" ]] || die 'No download-builds.sh found: $mydown' n
-[[ -n "$myd" && -d "$myd" ]] || die "$LINENO: Invalid working directory" n
-
+# Get list of packages.
 lst=
 nl="
 "
-
 while [[ -n "$1" ]] ; do
   lst="$lst$nl$1"
   shift
 done
 lst="`grep -v '^$' <<< "$lst"`"
 
-deb "lst = >>>\n$lst\n<<<\n"
-
-T=
-[[ -z "$TG" ]] || { T="gemspec" ; pre='rubygem-' ; }
-[[ -z "$p" ]] || { T="" ; pre='???-' ; }          # <<< [placeholder]
-
-[[ -n "$T" ]] || die 'No package type specified'
-
-deb "bra = '$bra'"
-deb "pre = '$pre'"
-
+# Width of first column
 while read z; do
   x='unknown'
   nam "$z"
@@ -157,12 +166,19 @@ while read z; do
   [[ $wx -gt $w1 ]] && w1=$wx
 done <<< "$lst"
 
-# padding for T
-Tp=$(printf "%-${#T}s" -)
+deb "lst = >>>\n$lst\n<<<\n"
+deb "bra = '$bra'"
+deb "pre = '$pre'"
+deb "fst = '$fst'"
 
-out '-------' '--------' "${Tp// /-}" '------------' '--------' '--------' ; put
-out 'package' 'specfile' "$T" 'licensecheck' 'licensee' 'oscrypto' ; put
-out '-------' '--------' "${Tp// /-}" '------------' '--------' '--------' ; put
+# Padding for T
+Tp=$(printf "%-${#T}s" -)
+out '-------' '--------' "${Tp// /-}" '------------' '--------' ; put
+out 'package' 'specfile' "$T" 'licensecheck' 'licensee' ; put
+out '-------' '--------' "${Tp// /-}" '------------' '--------' ; put
+#out '-------' '--------' "${Tp// /-}" '------------' '--------' '--------' ; put
+#out 'package' 'specfile' "$T" 'licensecheck' 'licensee' 'oscrypto' ; put
+#out '-------' '--------' "${Tp// /-}" '------------' '--------' '--------' ; put
 
 while read z; do
   x='unknown'
@@ -172,13 +188,19 @@ while read z; do
   out "$x"
   cd "$myd" || die "$LINENO: Failed to cd '$myd'" n
 
-  [[ -z "$fst" || -d "$z" ]] || { rhpkg co "$z" &>/dev/null ; }
-  [[ -d "$z" ]] || die "$LINENO: distgit directory '$z' not found" n
+  [[ -d "$z" ]] || {
+    [[ -z "$fst" ]] && {
+      die "$LINENO: distgit directory '$z' not found" n
+    } || {
+      rhpkg co "$z" &>/dev/null || die "$LINENO: failed to rhpkg co '$z'" n
+    }
+  }
   cd "$z" || die "$LINENO: failed to cd '$z'" n
 
   deb "`pwd`"
 
-  [[ "$fst" ]] && {
+  # Prepare git and sources and rpms
+  [[ -z "$fst" ]] || {
     `stx`
     git fetch &>/dev/null || die "$LINENO: failed to fetch"
     rhpkg switch-branch "$bra" &>/dev/null || {
@@ -195,7 +217,9 @@ while read z; do
     $mydown -b -x "$z" "el$r" &>/dev/null || err "$LINENO: failed to download RPMs"
     { set +x ; } &>/dev/null
   }
+  `stx`
 
+  # Spec file check
   f="$z.spec"
   [[ -r "$f" ]] || die "$LINENO: spec file '$f' missing"
   SF="`tr -s '\t' ' ' < "$f" | grep '^License:' | cut -d' ' -f2-`" || die "$LINENO: spec file license failed"
@@ -205,9 +229,8 @@ while read z; do
   out "`cut -d' ' -f3- <<< "$JSF"`"
 
   d=
-
   # type: gem
-  [[ "$TG" ]] && {
+  [[ -z "$TG" ]] || {
     y="`cut -d'-' -f2- <<< "$x"`"
     g="`ls ${y}-*.gem 2>/dev/null`"
     [[ -n "$g" && -r "$g" ]] || { err "$LINENO: gem file '$g' missing(not a rubygem?)" ; continue ; }
@@ -283,7 +306,6 @@ while read z; do
     licensecheck -c '.*' -i '' -l 200 -m -r * | tr -s '\t' ' ' | grep -vE ' (UNKNOWN|GENERATED FILE)$' \
       | xargs -n1 -i bash -c "c=0 ; while [[ \$c -lt 1000 ]]; do let 'c += 1' ; [[ -r \"\$(cut -d' ' -f-\${c} <<< '{}')\" ]] && { let 'c += 1' ; cut -d' ' -f\${c}- <<< '{}' ; exit 0 ; } ; done ; echo 'NOPE {}' >&2 ; exit 1" \
       | sort -u
-
   )
   out "`cut -d' ' -f3- <<< "$JSF"`"
 
@@ -296,5 +318,5 @@ while read z; do
 
 # temporarily disabled
 #  [[ "`oscryptocatcher . | grep content | grep -v '"content": \[\],'`" ]] && out "content" || out
-#  put
+  put
 done <<< "$lst"
