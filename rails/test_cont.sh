539687ee8dee4948606c175503e110d4ecd9d3cb
Pavel Valena <pvalena@redhat.com>
2018-08-30 10:15:17 +0200

    Add test_cont script to test ruby and rails images

---
+++ rails/test_cont.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -xe
+bash -n "$0"
+
+[[ "$1" == '-d' ]] && {
+  shift
+  debug='| tee -a /dev/stderr'
+}||:
+
+[[ "$1" == '-g' ]] && {
+  shift
+  gem="$1"
+  shift
+}||:
+
+[[ "$1" == '-l' ]] && {
+  shift
+  bundle='--local'
+}||:
+
+read -r -d '' BSC << EOS||:
+  set -xe
+  which rails || gem install rails
+  export GEM_HOME=\$( ruby -e 'puts Gem.user_dir' )
+  rails new app
+  cd app
+
+  [[ -n "$gem" ]] && echo "gem '$gem'" >> Gemfile
+  rm Gemfile.lock
+  bundle $bundle
+
+  ( rails s ; ) &
+  sleep 3
+
+  curl -Lks http://0.0.0.0:3000 $debug | head
+EOS
+
+exec docker run --rm -it "$1" /bin/bash -c "$BSC"
