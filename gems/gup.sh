e0a114d9a3f27cbb344fc3c0f0e3e37fb29198d8
Pavel Valena <pvalena@redhat.com>
2018-10-12 15:34:31 +0200

    gup: enhance bumpspec automation
    and make it more portable

--- gems/gup.sh
+++ gems/gup.sh
@@ -3,7 +3,7 @@
 EXT="gz|tgz|xz|gem"
 
 die () {
-  echo "--> Error: $1!" 2>&1
+  echo "--> Error: $1!" >&2
   exit 1
 }
 
@@ -36,63 +36,78 @@ ask () {
 
 } || ver=
 
- clean
- git fetch || die 'Failed to fetch git'
- git diff
- git log -p
- echo
- git status
- ask 'Reset repository'
+clean
+git fetch || die 'Failed to git fetch'
+git fetch origin || die 'Failed to git fetch origin'
 
- git stash || die 'Failed to stash git'
- git reset --hard origin/master || die 'Failed to reset git'
+git log -p
+echo
 
- fedpkg srpm &>/dev/null || die 'Failed to recreate old srpm'
- sn="$(basename -s '.src.rpm' "`ls *.src.rpm`")"
- clean
+git diff | colordiff
+echo
 
- ov="`rpmspec -q --qf '%{VERSION}\n' *.spec | head -1`"
- [[ "$ov" && "$ov" == "$(rev <<< "$sn" | cut -d'-' -f2 | rev)" ]] || die "Old version inconsistency- should be: '$ov'"
+git status
+ask 'Reset repository'
 
- nam="`rpmspec -q --qf '%{NAME}\n' *.spec | head -1`"
- [[ "$nam" ]] || die 'Bad NAME in *.spec'
- [[ "$nam" == "$(rev <<< "$sn" | cut -d'-' -f3- | rev)" ]] || die "Package name inconsistency- should be: '$nam'"
- grep '^rubygem-' <<< "$nam" && nam="`cut -d'-' -f2- <<< "$nam"`"
+git stash || die 'Failed to stash git'
+git reset --hard origin/master || die 'Failed to reset git'
 
- gem fetch "$nam" $ver || die "gem fetch failed"
- f="$(basename -s '.gem' "`ls *.gem`")"
- [[ "$f" && -r "$f.gem" ]] || die "Invalid or missing gem file: '$f'"
- [[ "$nam" == "`rev <<< "$f" | cut -d'-' -f2- | rev`" ]] || die "Failed gem name check of file: '$f'"
+fedpkg srpm &>/dev/null || die 'Failed to recreate old srpm'
+sn="$(basename -s '.src.rpm' "`ls *.src.rpm`")"
+clean
 
- xv="`rev <<< "$f" | cut -d'-' -f1 | rev`"
- [[ -z "$ver" ]] && ver="$xv"
- [[ "$ver" == "$xv" ]] || die "Version check failed: '$ver' vs '$xv'"
+X="`readlink -e *.spec`" || die 'Spec file not found'
 
- [[ "$ver" == "$ov" ]] && die "Version '$ver' is current"
+ov="`rpmspec -q --qf '%{VERSION}\n' "$X" | head -1`"
+[[ "$ov" && "$ov" == "$(rev <<< "$sn" | cut -d'-' -f2 | rev)" ]] || die "Old version inconsistency- should be: '$ov'"
+
+nam="`rpmspec -q --qf '%{NAME}\n' "$X" | head -1`"
+[[ "$nam" ]] || die 'Bad NAME in "$X"'
+[[ "$nam" == "$(rev <<< "$sn" | cut -d'-' -f3- | rev)" ]] || die "Package name inconsistency- should be: '$nam'"
+grep '^rubygem-' <<< "$nam" && nam="`cut -d'-' -f2- <<< "$nam"`"
+
+gem fetch "$nam" $ver || die "gem fetch failed"
+f="$(basename -s '.gem' "`ls *.gem`")"
+[[ "$f" && -r "$f.gem" ]] || die "Invalid or missing gem file: '$f'"
+[[ "$nam" == "`rev <<< "$f" | cut -d'-' -f2- | rev`" ]] || die "Failed gem name check of file: '$f'"
+
+xv="`rev <<< "$f" | cut -d'-' -f1 | rev`"
+[[ -z "$ver" ]] && ver="$xv"
+[[ "$ver" == "$xv" ]] || die "Version check failed: '$ver' vs '$xv'"
+
+[[ "$ver" == "$ov" ]] && die "Version '$ver' is current"
 
 [[ -d "$nam/" ]] && {
- echo
- ls "$nam/" || die "Failed to list '$nam/'"
- ask 'Remove directory'
- rm -rf "$nam/" || die "Failed to remove '$nam/'"
+  echo
+  ls "$nam/" || die "Failed to list '$nam/'"
+  ask 'Remove directory'
+  rm -rf "$nam/" || die "Failed to remove '$nam/'"
 }
 
- M="Update to $nam ${ver}."
- rpmdev-bumpspec -c "$M" -n "$ver" *.spec || die "Failed to bump spec with version '$ver' and message '$M'"
+M="Update to $nam ${ver}."
+c="rpmdev-bumpspec -c '$M'"
+bash -c "$c -n '$ver' '$X'" || {
+  bash -c "$c '$X'" \
+    && sed -i "s/^\(Version:\).*$/\1 $ver/" "$X" \
+    && sed -i "s/^\(Release:\)\s*[0-9]*\(.*\)$/\1 1\2/" "$X" \
+    || die "Failed to bump spec with version '$ver' and message '$M'"
+}
 
- gcom=' (tar|git|cp|mv|cd) '
- grep -A 5 ' git clone ' *.spec | grep '^#' |  grep -E "$gcom" \
-    | xargs -i bash -c "O=\$(sed -e 's|/|\\\/|g' <<< '{}') ; set -x ; sed -i \"/^\$O/ s/$ov/$ver/g\" *.spec"
+gcom=' (tar|git|cp|mv|cd) '
+grep -A 5 ' git clone ' "$X" | grep '^#' |  grep -E "$gcom" \
+  | xargs -i bash -c "O=\$(sed -e 's|/|\\\/|g' <<< '{}') ; set -x ; sed -i \"/^\$O/ s/$ov/$ver/g\" "$X""
 
- cmd=$( grep -A 3 '^# git clone ' *.spec | grep '^#' |  grep -E "$gcom" | cut -d'#' -f2- | xargs -i echo -n "{} && " \
-    | xargs -i echo "set -x ; {}echo Ok || exit 1" )
+cmd=$( grep -A 3 '^# git clone ' "$X" | grep '^#' |  grep -E "$gcom" | cut -d'#' -f2- | xargs -i echo -n "{} && " \
+  | xargs -i echo "set -x ; {}echo Ok || exit 1" )
 
- echo
- echo "\$cmd: $cmd"
- ask 'execute $cmd'
- bash -c "$cmd" || die 'Failed to execute $cmd'
+[[ -z "$cmd" ]] || {
+  echo
+  echo "\$cmd: $cmd"
+  ask 'execute $cmd'
+  bash -c "$cmd" || die 'Failed to execute $cmd'
+}
 
-for x in `spectool -A *.spec | grep ^Source | rev | cut -d' ' -f1 | cut -d'/' -f1 | rev` ; do
+for x in `spectool -A "$X" | grep ^Source | rev | cut -d' ' -f1 | cut -d'/' -f1 | rev` ; do
   { find -mindepth 2 -type f -name "$x" | xargs -n1 -i mv -v "{}" . ; } 1>&2
 
   [[ -r "$x" ]] || die "Source not found: $x"
@@ -100,24 +115,24 @@ for x in `spectool -A *.spec | grep ^Source | rev | cut -d' ' -f1 | cut -d'/' -f
   echo "SHA512 ($x) = `sha512sum "$x" | cut -d' ' -f1`"
 done > sources
 
- echo
- gem compare -bk "$nam" "$ov" "$ver"
- ask 'Continue'
+echo
+gem compare -bk "$nam" "$ov" "$ver"
+ask 'Continue'
 
- echo
- git status
- ask 'Continue'
+git commit -am "$M" || die "Failed to commit with message '$M'"
 
- git commit -am "$M" || die "Failed to commit with message '$M'"
+git show
 
- echo
- git show
- ask 'Run review'
+echo
+git status
+ask 'Continue'
 
- fedpkg srpm || die 'New fedpkg srpm failed'
+ask 'Run review'
 
- rev="$(readlink -f "`dirname "$0"`/rev.sh")"
- [[ -n "$rev" && -x "$rev" ]] || die "Invalid review script: '$rev'"
+fedpkg srpm || die 'New fedpkg srpm failed'
 
- clear
- exec "$rev" -f
+rev="$(readlink -f "`dirname "$0"`/rev.sh")"
+[[ -n "$rev" && -x "$rev" ]] || die "Invalid review script: '$rev'"
+
+clear
+exec "$rev" -f
