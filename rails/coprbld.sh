52335f71ffc417e5fa6f1739360adeb023b5703a
Pavel Valena <pvalena@redhat.com>
2018-12-11 12:23:17 +0100

    coprbld.sh: new rails build script for COPR

---
+++ rails/coprbld.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+bash -n "$0"
+set +e
+
+d="`pwd`"
+while read x; do
+  cd "$d" && cd "rubygem-${x}" && echo ">> $x" && {
+    [[ -r .built ]] && continue;
+    rm *.src.rpm;fedpkg --dist f30 srpm&&copr-cli build ruby-on-rails *.src.rpm&&touch .built
+  }
+done <<EOLX
+activesupport
+activejob
+activemodel
+activerecord
+actionmailer
+actioncable
+rails
+actionview
+actionpack
+railties
+EOLX
