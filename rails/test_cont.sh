d06a35cdb9ae8d76bbb9c540decd409777aab749
Pavel Valena <pvalena@redhat.com>
2019-03-28 09:01:15 -0400

    test_cont: enhanced output +debug; move to podman

--- rails/test_cont.sh
+++ rails/test_cont.sh
@@ -3,9 +3,17 @@
 set -xe
 bash -n "$0"
 
+debug='head'
+rm='--rm'
+dline=
+s='s'
+
 [[ "$1" == '-d' ]] && {
   shift
-  debug='| tee -a /dev/stderr'
+  debug='cat'
+  rm=
+  dline='echo " =================== "'
+  s=
 }||:
 
 [[ "$1" == '-g' ]] && {
@@ -16,24 +24,38 @@ bash -n "$0"
 
 [[ "$1" == '-l' ]] && {
   shift
-  bundle='--local'
+  blocal='--local'
 }||:
 
 read -r -d '' BSC << EOS||:
-  set -xe
+  set -x
+  ruby -v
+  gem list
+  gem env
+
   which rails || gem install rails
   export GEM_HOME=\$( ruby -e 'puts Gem.user_dir' )
-  rails new app
+  rails new app --skip-bundle --skip-spring --skip-test --skip-bootsnap -f
   cd app
 
   [[ -n "$gem" ]] && echo "gem '$gem'" >> Gemfile
   rm Gemfile.lock
-  bundle $bundle
+  bundle install --path vendor/bundle -r 3 $blocal
 
-  ( rails s ; ) &
-  sleep 3
+  { set +xe ; }&>/dev/null
+  $dline
 
-  curl -Lks http://0.0.0.0:3000 $debug | head
+  bash -c "set -x ; timeout 60 rails server puma -P rails.pid &>rails.log" &
+  sleep 31
+  PID="`cat rails.pid`"
+  grep '^[0-9]' <<< "$PID" || exit 1
+  $dline
+
+  curl -Lk$s 'http://0.0.0.0:3000' | $debug
+  sleep 30
+  $dline
+
+  cat rails.log
 EOS
 
-exec docker run --rm -it "$1" bash -c "$BSC"
+exec podman run $rm -it "$1" bash -c "$BSC"
