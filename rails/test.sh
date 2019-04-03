8bac69b3a375b54541fa73a963a2758626cc4783
Pavel Valena <pvalena@redhat.com>
2019-04-03 16:22:17 +0200

    rails/test: enhance

--- rails/test.sh
+++ rails/test.sh
@@ -23,6 +23,8 @@ trap 'kill 0 ; exit 0' SIGTERM
 trap 'kill 0 ; exit 0' SIGINT
 trap 'kill 0 ; exit 0' SIGABRT
 
+MAR='--bootstrap-chroot --new-chroot'
+
 die () {
   local ERR
   [[ "$1" ]] && ERR="$1" || ERR="Unknown"
@@ -66,25 +68,25 @@ usage () {
  # | xargs -n1 mock "$@" -n -qi
 
 [[ "$d" ]] && set -x
-[[ "$S" ]] && mock "$@" $Q $T --scrub=all
+[[ "$S" ]] && mock $MAR "$@" $T --scrub=all
 
 [[ "$C" ]] || {
-  mock "$@" $Q $T --clean || die 'Clean failed'
-  mock "$@" $Q $T --init || die "Init failed"
+  mock $MAR "$@" $T --clean || die 'Clean failed'
+  mock $MAR "$@" $T --init || die "Init failed"
 }
 
 [[ "$N" ]] || {
-  mock "$@" -n $Q $T --pm-cmd group install 'Ruby on Rails' || die 'group install failed'
+  mock $MAR "$@" -n $T --pm-cmd group install 'Ruby on Rails' || die 'group install failed'
 }
 
 [[ -n "$I" ]] && {
-  mock "$@" -n $Q $T -i $I || die 'additional install failed'
+  mock $MAR "$@" -n $T -i $I || die 'additional install failed'
 }
 
-mock "$@" -n $Q $T --unpriv --chroot "cd && rm -rf app/"
+mock $MAR "$@" -n $T --unpriv --chroot "cd && rm -rf app/"
 
-mock "$@" -n $Q $T --unpriv --chroot "cd && rails new app --skip-bundle --skip-spring --skip-test --skip-bootsnap -f" || die "rails new failed"
+mock $MAR "$@" -n $T --unpriv --chroot "cd && rails new app --skip-bundle --skip-spring --skip-test --skip-bootsnap -f" || die "rails new failed"
 
-#mock "$@" -n $Q $T --unpriv --chroot "cd && cd app && sed -i '/chromedriver-helper/ s/^/#/g' Gemfile" || die "Gemfile edits failed"
+#mock $MAR "$@" -n $T --unpriv --chroot "cd && cd app && sed -i '/chromedriver-helper/ s/^/#/g' Gemfile" || die "Gemfile edits failed"
 
-mock "$@" -n $Q $T --unpriv --chroot "cd && cd app && { ( timeout 20 rails s puma &> rails.log & ) ; sleep 5 ; curl -s http://0.0.0.0:3000 | tee -a /dev/stderr | grep -q '<title>Ruby on Rails</title>' && echo OK && exit 0 ; cat rails.log ; } ; exit 1" || die '`rails server` failed'
+mock $MAR "$@" -n $T --unpriv --chroot "cd && cd app && { ( timeout 20 rails s puma &> rails.log & ) ; sleep 5 ; curl -s http://0.0.0.0:3000 | tee -a /dev/stderr | grep -q '<title>Ruby on Rails</title>' && echo OK && exit 0 ; cat rails.log ; } ; exit 1" || die '`rails server` failed'
