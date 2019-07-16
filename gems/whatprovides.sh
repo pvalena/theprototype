5b87b2db2410224a88a9375b7829fe3127ab10ee
Pavel Valena <pvalena@redhat.com>
2019-07-16 17:33:40 +0200

    new: gems/whatprovides.sh -- what provides a gem

---
+++ gems/whatprovides.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# ./what---.sh [-a][-v][-d] GEM1 GEM2 GEM3
+#   Looks up dependencies using `dnf repoquery` in 'rawhide*' repos.
+#   You need to have `rawhide` repos installed.
+#   Auto-fills rubygem(GEM) or rubygem-GEM.
+#   Does lookup for both Requires and BuildRequires(`--arch=src`).
+#   Outputs only entries with upper bound contraint(`<`).
+#
+#   ! Run it as a same user you run dnf(f.e. with sudo) !
+#
+#   Options; expected in alpabetic order:
+#     -a  all entries (debug output)
+#     -d  debug output (all entries)
+#     -v  verbose output (all constraints)
+#
+
+set -e
+bash -n "$0"
+
+xdnf="dnf -q --disablerepo='*' --enablerepo='rawhide*'"
+D=provides
+C=what$D
+
+[[ "$1" == '-a' ]] && { DEBUG=y ; shift ; } ||:
+[[ "$1" == '-d' ]] && { DEBUG=y ; shift ; } ||:
+[[ "$1" == '-v' ]] && { VERBO=y ; shift ; } ||:
+[[ "${1:0:1}" != '-' ]] || { echo "Invalid arg: $1" ; exit 1 ; }
+
+for g in "$@"; do
+  echo -e "\n--> $g"
+
+  for a in '' --arch=src ; do
+    for z in -$g "($g)"; do
+      bash -c "$xdnf repoquery ${a} --${C} 'rubygem${z}'"
+    done
+  done \
+  | rev | cut -d'-' -f3- | rev | sort -u
+
+  :\
+  | xargs -i bash -c "
+    O=\"\$($xdnf repoquery --${D} '{}' | grep '${g}')\"
+
+    [[ -z \"$DEBUG\" ]] && {
+      # NOT Debug
+      [[ -n \"$VERBO\" ]] && {
+        O=\"\$(grep '[><=]' <<< \"\$O\")\"
+        :
+      } || O=\"\$(grep '<' <<< \"\$O\")\"
+
+      [[ -z \"\$O\" ]] && exit
+    }
+
+    echo -e \"{}:\n\$O\n\"
+  "
+done
