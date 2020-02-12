d7968729ce788e476d026bcc624718c79ce8d036
Pavel Valena <pvalena@redhat.com>
2020-02-12 19:43:59 +0100

    cr-build: retry support, better status handling, more resiliency
    also require 'with' handling.

--- pkgs/cr-build.sh
+++ pkgs/cr-build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-set -xe
+set -e
 bash -n "$0"
 
 COPR_URL="https://copr-be.cloud.fedoraproject.org/results/pvalena/"
 
 d=lss
+[[ "$1" == '-d' ]] && set -x && shift
 [[ "$1" == '-c' ]] && d=cat && shift
-[[ "$1" == '-s' ]] && {
+[[ "$1" == '-d' ]] && set -x && shift
+[[ "$1" == '-r' ]] && {
+  RETRY="$2"
+  grep -qE "^[0-9]+$" <<< "$RETRY"
+  shift 2
+  :
+} || RETRY=0
+[[ "$1" == '-s' ]] && shift || {
   rm *.src.rpm ||:
-  shift
-}||:
+}
 
 [[ "$1" == '-w' ]] && {
   shift
@@ -17,8 +24,7 @@ d=lss
   shift
 
   while copr-cli status $P \
-      | tee -a /dev/stderr \
-      | grep -qv succeeded ; do
+      | grep -qvE '(succeeded|failed|cancelled)' ; do
     sleep 300
   done
 }||:
@@ -32,8 +38,13 @@ x="${2}"
 p="$(basename "$PWD")"
 [[ -n "$p" ]]
 
-l="`readlink -e "../copr-r8-${n}"`"
-[[ -n "$l" && -d "$l" ]]
+l="`readlink -f "../copr-r8-${n}"`"
+# don't
+# mkdir -p "$l"
+[[ -n "$l" && -d "$l" ]] || {
+  echo "Error: log directory '$l' does not exist" >&2
+  exit 1
+}
 
 f="${l}/${p}.log"
 touch "$f"
@@ -43,31 +54,41 @@ ls *.src.rpm &>/dev/null || {
     echo "Warning: modifying spec file..." >&2
     sed -i 's/^Recommends: /Requires: /' *.spec
     sed -i '/^Suggests: / s/^/#/' *.spec
+    sed -i -e 's/\(Requires\:\)\s*(.*with\(.*\))/\1\2/' *.spec
     fedpkg --dist f31 srpm
   }
 }
 
-{ set +xe ; } &>/dev/null
-date -Isec
+echo -ne "\e]2;C:$p\a"
 
-O="`copr-cli build $n *.src.rpm 2>&1 | tee /dev/stderr`" && R=0 || R=1
+set +e -o pipefail
+nr=0
+while :; do
+  O="`date -Isec; copr-cli build $n *.src.rpm 2>&1 | tee -a /dev/stderr`"
+  R=$?
 
-b="`echo "$O" | grep '^Created builds: ' | cut -d' ' -f3`"
-[[ -n "$b" ]] || exit 1
-grep -qE '^[0-9]*' <<< "$b" || exit 3
+  b="`echo "$O" | grep '^Created builds: ' | cut -d' ' -f3`" ||:
+  [[ -n "$b" ]] || exit 1
+  grep -qE '^[0-9]*' <<< "$b" || exit 3
 
-[[ -t 1 ]] || d=cat
-[[ -t 0 ]] || d=cat
+  [[ -t 1 ]] || d=cat
+  [[ -t 0 ]] || d=cat
+  [[ $R -eq 0 || $nr -lt $RETRY ]] && d=cat
 
-sleep 2
-
-grep -q succeeded <<< "$O" || {
-  (
+  sleep 15
+  {
     echo "$O"
     u="${COPR_URL}${n}/${x}/`printf "%08d" $b`-${p}/builder-live.log.gz"
+    echo "> $u"
     curl -sLk "$u" | zcat | uniq
 
-  ) 2>&1 | tee "$f" | $d
-}
+  } 2>&1 | tee "$f" | $d
 
-exit $R
+  [[ $R -eq 0 || $nr -ge $RETRY ]] && exit $R || {
+    let 'nr += 1'
+    echo -e "\n> Retrying ($nr/$RETRY) ..."
+  }
+done
+
+echo "You shouldn't see this..." >&2
+exit 1
