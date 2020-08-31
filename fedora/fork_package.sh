4ba625501fe15e61ac4aa92dea4a2b919731c84f
Pavel Valena <pvalena@redhat.com>
2020-08-31 10:47:52 +0200

    fork_package +debug

--- fedora/fork_package.sh
+++ fedora/fork_package.sh
@@ -16,7 +16,9 @@ abort () {
 
 while [[ -n "$1" ]]; do
   repo="$1"
+  [[ -n "$DEBUG" ]] && set -x
   curl -s -X POST -H "Authorization: token $TOKEN" -d "repo=$repo&namespace=rpms&wait=1" "${SRC_FPO_RPMS_FORK}"
+  { [[ -n "$DEBUG" ]] && set +x ; } &>/dev/null
   echo
   shift
 done
