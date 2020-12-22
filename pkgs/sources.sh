bd07b8ee6d306defe5b4195737fc65d0e6c58de0
Pavel Valena <pvalena@redhat.com>
2020-12-22 20:39:48 +0100

    pkgs/sources.sh: add common regex var
    also ignore '.stp'.

--- pkgs/sources.sh
+++ pkgs/sources.sh
@@ -82,10 +82,12 @@ find -L -mindepth 2 -maxdepth 3 -type f -name '*.txz' -o -name '*.tgz' | xargs -
 
 spectool -S "$X" | grep ^Source | cut -d' ' -f2 | grep -E '^http[s]*://' | xargs -r -P 0 curl -sLO
 
-for x in `spectool -S "$X" | grep ^Source | rev | cut -d' ' -f1 | cut -d'/' -f1 | rev | grep -vE '(\.(rb|js|patch|1|sh)|LICENSE|binstub|rubygems\..*|macros\..*)$'` ; do
+regex='(\.(rb|js|patch|1|sh|stp)|LICENSE|binstub|rubygems\..*|macros\..*)$'
+
+for x in `spectool -S "$X" | grep ^Source | rev | cut -d' ' -f1 | cut -d'/' -f1 | rev | grep -vE "$regex"` ; do
   echo "SHA512 ($x) = `sha512sum "$x" | cut -d' ' -f1`" | tee -a /dev/stderr
 done > sources
 
-for x in `spectool -S "$X" | grep -E '^(Source|Patch)' | rev | cut -d' ' -f1 | cut -d'/' -f1 | rev | grep -E '(\.(rb|js|patch|1|sh)|LICENSE|binstub|rubygems\..*|macros\..*)$'` ; do
+for x in `spectool -S "$X" | grep -E '^(Source|Patch)' | rev | cut -d' ' -f1 | cut -d'/' -f1 | rev | grep -E "$regex"` ; do
   git add "$x"
 done
