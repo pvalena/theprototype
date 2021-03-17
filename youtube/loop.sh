02575c60ee45d01409d7e57cde7b1d593076a46f
Pavel Valena <pvalena@redhat.com>
2021-03-17 15:14:41 +0100

    youtube/loop.sh: Fixup path.

--- youtube/loop.sh
+++ youtube/loop.sh
@@ -2,7 +2,7 @@
 
 while date -Isec; do
 
-  ./run.rb -s 'DC_2021_events.csv' 'DC_2021_links.csv' "/run/media/lpcs/Seagate Expansion Drive/Media/DevConfCZ_2021-processed/zasilkovna"
+  ./run.rb -s 'DC_2021_events.csv' 'DC_2021_links.csv' "/run/media/lpcs/Seagate Expansion Drive/Media/DevConfCZ_2021-processed/upload"
    2>&1 \
     | tee -a "logs/upload_`date -Ins`.log"
 
