9b6c60b6150e2aa2992269d3d3c1369f8e7b9b69
Pavel Valena <pvalena@redhat.com>
2021-10-08 16:14:45 +0200

    get_pr_comments.sh: add verified maintainers

--- fedora/get_pr_comments.sh
+++ fedora/get_pr_comments.sh
@@ -13,7 +13,7 @@ com="${myd}/get_pr_common.sh"
   FROM="$1"
   shift
   :
-} || FROM="pvalena vondruch jaruga decathorpe ilgrad"
+} || FROM="pvalena vondruch jaruga decathorpe ilgrad stevetraylen mtasaka"
 
 sel=""
 for n in ${FROM}; do
