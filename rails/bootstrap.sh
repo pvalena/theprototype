f8b6bfb171785a76e59049bf5ad896805c7c251c
Pavel Valena <pvalena@redhat.com>
2021-10-08 16:31:33 +0200

    rails/bootstrap.sh: force push changes if diff is empty

--- rails/bootstrap.sh
+++ rails/bootstrap.sh
@@ -16,4 +16,10 @@ grep -r '^%bcond_without bootstrap$' -l \
     sed -i 's/^%bcond_without bootstrap/%bcond_with bootstrap/' *.spec
     git commit -am '$M'
     rm .built
+
+    # Is the remote diff empty, a part from the above change?
+    D=\"\$(git diff \"pvalena/\$gitb | grep '^* ' | cut -d' ' -f2-)\" | grep -vE '^(\+|\-)%bcond_with' | grep -v '^ ' | grep -v '^@@ ' | grep -v '^\-\-\- ' | grep -v '^index ' | grep -v '^diff ' | grep -v '^\+\+\+ ')\"
+    [[ -z \"\${D}\" ]] && git push -f
   "
+
+exit 0
