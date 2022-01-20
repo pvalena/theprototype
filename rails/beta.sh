b9b237066a48aa1c0d1f43f78699b7375aa0f654
Pavel Valena <pvalena@redhat.com>
2022-01-20 15:56:23 +0100

    Add rails/beta.sh

---
+++ rails/beta.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -xe
+bash -n "$0"
+
+[[ -n "$1" ]] || {
+  find . -maxdepth 2 -type f -iname '*.spec' \
+    | xargs -r "$0"
+
+  exit "$?"
+}
+
+OLD_VERSION="${1:-6.0.0}"
+VERSION="${2:-6.0.2}"
+BETA="${3:-.rc1}"
+
+myd="`pwd`"
+for sf in "$@"; do
+  set -e
+  f="$(basename "$sf")"
+  d="$(dirname "$sf")"
+
+  cd "$myd"
+  [[ -d "$d" ]] && cd "$d" ||:
+  [[ -r "$f" ]] || exit 2
+
+  g="$(cut -d'-' -f2- <<< "$f" | cut -d'.' -f1)"
+
+  # spec file edits
+  [[ -z "$OLD_VERSION" ]] || {
+    sed -i "s/${OLD_VERSION}/${VERSION}${BETA}/g" "$f"
+  }
+
+  sed -i "s/^\(Version:\).*$/\1 ${VERSION}/" "$f"
+  sed -i "s/^\(Release:\).*$/\1 1%{?dist}/" "$f"
+
+  sed -i 's/%{version}/%{version}%{?prerelease}/g' "$f"
+  sed -i 's/%{?prerelease}%{?prerelease}/%{?prerelease}/g' "$f"
+  sed -i '/^\s*BuildRequires:/ s/%{?prerelease}//g' "$f"
+  sed -i '/^\s*Requires:/ s/%{?prerelease}//g' "$f"
+
+  sed -i '/^\s*%global prerelease/ d' "$f"
+  [[ -z "$BETA" ]] || {
+    sed -i "/^%global gem_name/ a \
+      `echo -e "\n"` \
+      %global prerelease ${BETA}" "$f"
+  }
+
+  # use git archive and .txz
+  sed -i "s/git checkout \(\S*\) && tar \S* \(\S*\)\.\S*/git archive -v -o \2.txz \1/" "$f"
+  sed -i "s/&& git checkout \S*/--no-checkout/" "$f"
+  sed -i "s/&& cd \S*/--no-checkout/" "$f"
+  sed -i "s/--no-checkout --no-checkout/--no-checkout/" "$f"
+  sed -i "s/tar \S* \(\S*\)\.\S*/git archive -v -o \1.txz v???/" "$f"
+  sed -i 's/^\s*\(Source\S*\)\s*\(\S*\)\.t\S*/\1 \2.txz/' "$f"
+
+  # done
+  set +ex
+
+  # cleanup
+  #{ rm *.src.rpm ; rm *.gem ; rm *.tgz ; rm *.tar.gz ; } &>/dev/null
+
+  # fetch the new gem version
+  #gem fetch --pre "$g"
+
+  ## sources
+  # run the command get ~magic~
+  ## TODO ##
+done
+
+exit 0
