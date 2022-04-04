612cc6b567066b8a42d36f18400000c4fb1932f3
Pavel Valena <pvalena@redhat.com>
2022-04-04 16:20:05 +0200

    new: ci_waive.sh - waiverdb REST API call

---
+++ fedora/ci_waive.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# ./ci_waive.sh [-d] [-r EL] PACKAGE TESTCASE COMMENT
+#
+
+set -e
+
+n=0
+arg () {
+  #local A="$(printf "%q" "$1")"
+  A="$(sed -e 's/"/'\''/g'<<< "$1")"
+  let "n += 1"
+  [[ -n "$A" ]] || exit ${n:-1}
+
+  echo "$A"
+}
+
+[[ "$1" == "-d" ]] && {
+  set -x
+  shift
+}
+
+[[ "$1" == "-r" ]] && {
+  EL="$2"
+  shift 2
+  :
+} || EL=9
+
+PACKAGE="$(arg "$1")"
+shift
+
+TESTCASE="$(arg "$1")"
+shift
+
+COMMENT="$(arg "$1")"
+shift
+
+curl --negotiate -u: -X POST https://waiverdb.engineering.redhat.com/api/v1.0/waivers/ \
+  -d "{\"subject_type\": \"brew-build\", \"subject_identifier\": \"${PACKAGE}\", \"testcase\": \"${TESTCASE}\", \"waived\": true, \"product_version\": \"rhel-${EL}\", \"comment\": \"${COMMENT}\"}"
